5d0124c3c8e3ff6bb08ffd6176a60ddf2992aa5c
jbcruz24 <123082972+jbcruz24@users.noreply.github.com>
2023-03-12 16:32:24 +0800

    Add files via upload

--- EDC/src/main/java/SignUpServlet.java
+++ EDC/src/main/java/SignUpServlet.java
@@ -25,14 +25,18 @@ public class SignUpServlet extends HttpServlet {
 		private String dbDriver = "com.mysql.cj.jdbc.Driver";
 		
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String name = request.getParameter("name");
+   		String role = request.getParameter("role");
+   		String username = request.getParameter("uname");
+   		String password = request.getParameter("upwd");
+		
+		if(email != "" && name != "" && role != "" && username != "" && password != "") {
 		try {
 			Class.forName(dbDriver);
 			Connection con = DriverManager.getConnection(dbUrl, dbUname,dbPassword);
-			String email = request.getParameter("email");
-	   		String name = request.getParameter("name");
-	   		String role = request.getParameter("role");
-	   		String username = request.getParameter("uname");
-	   		String password = request.getParameter("upwd");
+			
+	   		
 	   		
 	   		String EncEmail = AES.encrypt(email);
 	   		String EncName = AES.encrypt(name);
@@ -59,6 +63,10 @@ public class SignUpServlet extends HttpServlet {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		}else{
+			response.sendRedirect("SignUp.jsp");
+			
 		}
 	}
 
